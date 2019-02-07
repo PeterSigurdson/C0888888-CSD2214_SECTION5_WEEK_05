7731b59a1dfbf966939da15d3f997a7376dc7025
peter sigurdson <peter@petersigurdson.net>
2019-02-07 11:54:20 -0500

    final commit

--- C0888888/js/main.js
+++ C0888888/js/main.js
@@ -5,9 +5,18 @@
 
 function arrays() {
     var pens;
-    pens = ["red", "orange", "green", "blue", "purple"];
-    console.log(pens);
+    pens = new Array("red", "orange", "green", "blue", "purple");
+    markers = pens;
+    console.log(markers);
+    console.log(markers[0]);
+    for (i = 0; i < markers.length; i++) {
+        alert(markers[i]);
+    }
+    alert("Length of array is : " + markers.length);
+
 }
+
+
 
 
 
