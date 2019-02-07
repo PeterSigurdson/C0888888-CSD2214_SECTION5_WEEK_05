267052ef10b351a80ef5cd55febbd9fcac5e9652
peter sigurdson <peter@petersigurdson.net>
2019-02-07 11:41:26 -0500

    11:45 am commit

--- C0888888/js/main.js
+++ C0888888/js/main.js
@@ -1,1 +1,15 @@
-﻿// Your code here!
+﻿function myFunction(a, b)
+{
+    return a+b;   // The function returns the product of p1 and p2
+}
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "green", "blue", "purple"];
+    console.log(pens);
+}
+
+
+
+
+ 
